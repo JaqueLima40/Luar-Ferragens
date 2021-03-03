f37ebefa5edd4924318c5511dda5c8f76a82b28c
JAQUELINE BATISTA DE LIMA OLIVEIRA <005716105@itaud.des.ihf>
2021-03-03 14:22:05 -0300

    versionamento para meu primenrio site em produção

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Luarferragens</title>
+</head>
+<body>
+
+<h1>Luarferragens</h1>
+<p>Produtos de ferragens para seus projetos de casa e construção.</p>
+
+</body>
+</html>
