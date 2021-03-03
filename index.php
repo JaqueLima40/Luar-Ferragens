d97dda5adec10ff1b01dea2c017783ecd3409d84
JAQUELINE BATISTA DE LIMA OLIVEIRA <005716105@itaud.des.ihf>
2021-03-03 16:41:14 -0300

    Trabalhando cores

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Luarferragens</title>
-</head>
-<body>
-
-<h1>Luarferragens</h1>
-<p>Produtos de ferragens para seus projetos de casa e construção.</p>
-
-</body>
+   <head>
+      <title>Luarferragens</title>
+   </head>
+   <body>
+      <h1>Luarferragens</h1>
+      <p>Produtos de ferragens para seus projetos de casa e construção.</p>
+      <div style="width:100%;display:flex;height: 80px">
+      <div style="width:50%;background: blue"></div>
+      <div style="width:50%;background: red"></div>
+      <div>
+   </body>
 </html>
